6087c92af9ab045b832b8509557ad18a94bba663
Chris Freeman <cwfreeman@gmail.com>
2010-08-28 08:40:21 -0500

    Simplify workings to ease testing. Significantly simplify design as a result.

--- makefile
+++ makefile
@@ -3,6 +3,8 @@ include $(GOROOT)/src/Make.$(GOARCH)
 TARG=refactor
 GOFILES=\
 	rename_symbol.go
+#	refactor_visitor.go
+#	symbol_table.go
 
 
 include $(GOROOT)/src/Make.pkg
--- rename_symbol_test.go
+++ rename_symbol_test.go
@@ -4,12 +4,37 @@ import (
 	"testing"
 )
 
-func TestFindsNameOfVariableAtPosition(t *testing.T) {
-	var src = RefactorSource("var myString = \"Hello World\";")
+const (
+	SIMPLE_DECL = "var myString = \"Hello World\";"
+	VAR_NAME = "myString"
+	VAR_DECL_START_POS = 5
+)
 
-	var varDecl = src.GetVariableNameAt(4);
+func CallGetVariableNameAt(pos int) string {
+	src := RefactorSource(SIMPLE_DECL)
 
-	if varDecl != "myString" {
+	return src.GetVariableNameAt(pos);
+}
+
+func TestFindsNameOfVariableDeclStartingAtPosition(t *testing.T) {
+	varName := CallGetVariableNameAt(VAR_DECL_START_POS)
+
+	if varName != VAR_NAME {
+		t.Fail()
+	}
+}
+
+func TestFindsNameOfVariableDeclContainingPosition(t *testing.T) {
+	varName := CallGetVariableNameAt(VAR_DECL_START_POS);
+
+	if varName != VAR_NAME {
+		t.Fail()
+	}
+}
+
+func TestDoesntFindNameOfVariableDeclAfterPosition(t *testing.T) {
+	varName := CallGetVariableNameAt(VAR_DECL_START_POS - 2)
+	if varName != "" {
 		t.Fail()
 	}
 }
